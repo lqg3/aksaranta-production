bbf16ecb2b69b4f1de10d449e552db2d6e16a96f
jovanka07 <jovanka0709@gmail.com>
2025-07-11 23:50:28 +0700

    virtual-tour

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\VirtualTourController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/virtual', [VirtualTourController::class, 'index']);
